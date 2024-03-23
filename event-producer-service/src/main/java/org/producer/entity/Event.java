a0368b11656635e266fbbbbcb825be5ab28f234b
SergeyPodgorny <serkonpod@gmail.com>
2024-03-23 16:42:11 +0300

    event entity class adjusted

--- event-producer-service/src/main/java/org/producer/entity/Event.java
+++ event-producer-service/src/main/java/org/producer/entity/Event.java
@@ -1,14 +1,31 @@
 package org.producer.entity;
 
 
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.ZonedDateTime;
 
 @Entity
+@Table(name = "events")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Event {
 
     @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name ="uuid", strategy ="uuid2")
     private String id;
-
+    private String variousPayload;
+    private Boolean isTransactionalFailing;
+    private ZonedDateTime timeOfAttempt;
+    private ZonedDateTime timeOfTransaction;
 
 }
