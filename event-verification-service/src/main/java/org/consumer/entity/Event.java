a7d182f167bc5bd47eeb748dfc33cbed741506cd
SergeyPodgorny <serkonpod@gmail.com>
2024-03-24 07:50:22 +0300

    saving work

---
+++ event-verification-service/src/main/java/org/consumer/entity/Event.java
@@ -0,0 +1,34 @@
+package org.consumer.entity;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "events")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Event {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name ="uuid", strategy ="uuid2")
+    private String id;
+    private String variousPayload;
+    private Boolean isTransactionalFailing;
+    private String timeOfAttempt;
+
+
+
+
+}
